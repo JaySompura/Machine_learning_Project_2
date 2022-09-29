031d9cb54efb026ef6084bac4edb5b7e8abc08f3
Jay Sompura <jaynsompura20@gmail.com>
2022-09-29 18:06:49 +0530

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
-    return "hello world"
+    return "CI/CD pipeline has been established"
     
 
 if __name__ == '__main__':
