f19a7531a53abfd24b7d74cb7253ff114d713659
Jay Sompura <jaynsompura20@gmail.com>
2022-10-03 12:29:41 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 #Declaring variables for setup functions
@@ -19,7 +19,8 @@ def get_requirements_list()->List[str]:
 
     """
     with open (REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_file.readline()
+        return requirement_file.readline()
+        
 
 
 setup(
@@ -27,7 +28,7 @@ name = PROJECT_NAME,
 version = VERSION,
 author = AUTHOR,
 description = DESCRIPTION,
-packages=PACKAGES,
+packages=find_packages(),
 install_requires = get_requirements_list()
 
 )
